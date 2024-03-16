2e5f95d14930e76ba6366ef452848dab45fe2f01
AbhinavGupta-012 <abhinavgupta1702@gmail.com>
2024-03-16 22:07:59 +0530

    Made the code more readable and fixed visual bugs

--- hangman.py
+++ hangman.py
@@ -9,6 +9,8 @@ def face():
     print("\t\t\t\t|")
     print("\t\t\t\t|")
     print("\t\t\t             ======")
+
+
 def neck():
     print("\t\t\t+-----------+")    
     print("\t\t\tO\t|")
@@ -17,41 +19,47 @@ def neck():
     print("\t\t\t\t|")
     print("\t\t\t\t|")
     print("\t\t\t             ======")
-def lefthand():
+
+
+def left_hand():
     print("\t\t\t+-----------+")    
     print("\t\t\tO\t|")
-    print("\t\t\t/|\t|")
+    print("\t\t       /|\t|")
     print("\t\t\t\t|")
     print("\t\t\t\t|")
     print("\t\t\t\t|")
     print("\t\t\t             ======")
 
-def righthand():
+
+def right_hand():
     print("\t\t\t+-----------+")    
     print("\t\t\tO\t|")
-    print("\t\t\t/|\\\t|")
+    print("\t\t       /|\\\t|")
     print("\t\t\t\t|")
     print("\t\t\t\t|")
     print("\t\t\t\t|")
     print("\t\t\t             ======")
 
-def leftleg():
+
+def left_leg():
     print("\t\t\t+-----------+")    
     print("\t\t\tO\t|")
-    print("\t\t\t/|\\\t|")
-    print("\t\t\t/\t|")
+    print("\t\t       /|\\\t|")
+    print("\t\t       /\t|")
     print("\t\t\t\t|")
     print("\t\t\t\t|")
     print("\t\t\t             ======")
 
-def rightleg():
+
+def right_leg():
     print("\t\t\t+-----------+")    
     print("\t\t\tO\t|")
-    print("\t\t\t/|\\\t|")
-    print("\t\t\t/\\\t|")
+    print("\t\t       /|\\\t|")
+    print("\t\t       / \\\t|")
     print("\t\t\t\t|")
     print("\t\t\t\t|")
     print("\t\t\t             ======")
+
 
 #structure of hanging
 def structure():
@@ -63,79 +71,74 @@ def structure():
     print("\t\t\t\t|")
     print("\t\t\t             ======")
 
+
 #defining the words using random
-word=int(random.random()*4)
-if word==1:
-    words = 'rhino'
-elif word==2:
-    words='hey'
-elif word==3:
-    words='red'
+word = int(random.random()*4)
+if word == 1:
+    words = "rhino"
+elif word == 2:
+    words = "hey"
+elif word == 3:
+    words = "red"
 else:
-    words='krishna'
+    words = "krishna"
 
 #creating a block of underscores of length of the given word
-o=[]
-paap=[]
+o = []
 for i in range(len(words)):
-    ab='_'
+    ab = '_'
     o.append(ab)
-paap_ka_ghada=0
+hang_man = 0
 
 #2nd Version
 #PRINTING THE SET OF INSTRUCTIONS
-print("#Some Words are prefixed and you have to guess them.")
-print("#If the guessed word is correct, then the blank is replaced by the word!")
-print("#Otherwise your chances are reduced (Maximum Wrong Guesses: 6)")
+print("THE HANGMAN")
+print("# Some Words are prefixed and you have to guess them.")
+print("# If the guessed word is correct, then the blank is replaced by the word!")
+print("# Otherwise your chances are reduced (Maximum Wrong Guesses: 6)")
 #printing the block of underscores
 structure()
+
 for i in o:
-    print(i,end=' ')
+    print(i, end=' ')
 
-wrong_words=[]
-
+wrong_words = []
 
 #playing the real game 
-for u in range(len(words)+6):
-    s=input("\nGuess A Word: ")
+for u in range(len(words) + 6):
+    s = input("\nGuess A Word: ")
     for t in s:
-        if s in ['0','1','2','3','4','5','6','7','8','9']:
-            rightleg()
+        if s in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            right_leg()
             print('You Are Terminated')
             break
     if s in words:
         o.pop(words.index(s))
-        o.insert(words.index(s),s)
+        o.insert(words.index(s), s)
     else:
         wrong_words.append(s)
-        paap_ka_ghada=paap_ka_ghada+1
-        if(paap_ka_ghada==1):
+        hang_man = hang_man + 1
+        if(hang_man == 1):
             face()
             wrong_words.append(s)
-        elif(paap_ka_ghada==2):
+        elif(hang_man == 2):
             neck()
-        elif(paap_ka_ghada==3):
-            lefthand()
-        elif(paap_ka_ghada==4):
-            righthand()
-        elif(paap_ka_ghada==5):
-            leftleg()
+        elif(hang_man == 3):
+            left_hand()
+        elif(hang_man == 4):
+            right_hand()
+        elif(hang_man == 5):
+            left_leg()
         else:
-            rightleg()
+            right_leg()
             print("YOU ARE TERMINATED")
             break
     for i in wrong_words:
-            print(i,end='  ')
+            print(i, end='  ')
     print('\n')
     for i in o:
-        print(i,end="  ")
+        print(i, end="  ")
     if '_' not in o:
         print("\nCongratulations! You Have Completed the Game")
         break
     
-
-
-
-
-
-
